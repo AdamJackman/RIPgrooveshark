0578ffdf49e83385770bb7685b98a1e8905f83f2
AdamJackman <adam.jackman@mail.utoronto.ca>
2015-04-03 00:56:18 -0400

    Final functionality touches

--- ext/js/popup.js
+++ ext/js/popup.js
@@ -14,7 +14,6 @@ function saveListsExec(){
 
 function findDiff(){
 	//Send an ajax request in order to start the find functionality
-	//newwindow = window.open("http://ec2-52-0-6-182.compute-1.amazonaws.com/rip.php?type=init", "_blank", "resizable=yes, scrollbars=yes, titlebar=yes, width=500, height=500, top=10, left=10");
 	$.ajax({
 		type: 'GET',
 		url: "http://ec2-52-0-6-182.compute-1.amazonaws.com/rip.php?jsoncallback=?",
@@ -23,19 +22,64 @@ function findDiff(){
 			"type": "find"
 		},
 		success: function(response){
-			alert("Success");
-			alert(response);
+			//Here is where you would really like to show the difference to the user
+			if(response){
+				var res = JSON.parse(response);	
+				displayMissing(res);
+			}
+			
+
 		},
 		error: function(){
 		}
 	});
 }
 
+function displayMissing(res){
+	//Create the div to show all the missing in
+	var missingBox = document.createElement('div');
+	missingBox.setAttribute('id', "missingBox");
+	//add a label
+	var missingLabel = document.createElement('label');
+	missingLabel.setAttribute('class', 'mainMissLabel');
+	missingLabel.innerHTML = 'Missing Songs';
+	missingBox.appendChild(missingLabel);
+
+	var i;
+	for (i=0; i<res.length; i++){
+		//create the Div
+		var missingDiv = document.createElement('div');				
+		missingDiv.setAttribute('class', 'missItem');
+		//create the labels
+		var songLabel = document.createElement('label');
+		songLabel.setAttribute('class', 'missSong');
+		songLabel.innerHTML = res[i]['songName'];
+		var artistLabel = document.createElement('label');
+		artistLabel.innerHTML = " By: " + res[i]['artistName']
+		//add Labels to Div and Div to the main
+		missingDiv.appendChild(songLabel);
+		missingDiv.appendChild(artistLabel);
+		missingBox.appendChild(missingDiv);
+	}
+
+	//add a button to get rid of text
+	var remBut = document.createElement('button');
+	remBut.setAttribute('id', 'noteBut2');
+	remBut.setAttribute('class', 'btn btn-info pull-right');
+	remBut.setAttribute('type', 'button');
+	remBut.innerHTML = "Done";
+	remBut.onclick = function(){
+		$('#missingBox').remove();
+	}
+	missingBox.appendChild(remBut);
+
+	//add the missingBox onto the main html
+	$('body').append(missingBox);
+}
+
 function saveLists(){
 	//send an ajax request in order to start the save functioality
-	//newwindow = window.open("http://ec2-52-0-6-182.compute-1.amazonaws.com/rip.php?type=init", "_blank", "resizable=yes, scrollbars=yes, titlebar=yes, width=500, height=500, top=10, left=10");
-	//newwindow2 = window.open("http://ec2-52-0-6-182.compute-1.amazonaws.com/rip.php?type=init", "_blank", "resizable=yes, scrollbars=yes, titlebar=yes, width=500, height=500, top=10, left=10");
-		$.ajax({
+	$.ajax({
 		type: 'GET',
 		url: "http://ec2-52-0-6-182.compute-1.amazonaws.com/rip.php?jsoncallback=?",
 		timeout: 0,
@@ -43,7 +87,7 @@ function saveLists(){
 			"type": "save"
 		},
 		success: function(response){
-			alert("Success");
+			notifyMessage("Successfully saved your playlists");
 		},
 		error: function(){
 		}
@@ -63,7 +107,6 @@ function auth(type){
 		success: function(response){
 			if(response == 'logged'){
 				//call the function
-				alert("logged spotted");
 				if (type == 'find'){
 					findDiff();
 				}
@@ -71,7 +114,7 @@ function auth(type){
 					saveLists();
 				}
 				else{
-					alert("something terrible has happened");
+					alert("I don't know how you got here, but I'm impressed");
 				}
 			}
 			else{
@@ -80,4 +123,28 @@ function auth(type){
 			}
 		}
 	});
+}
+
+function notifyMessage(msg){
+
+	var alertDiv = document.createElement('div');
+	alertDiv.setAttribute('id', "alerterDiv");
+	alertDiv.setAttribute('class', "alerter");
+	$('body').append(alertDiv);
+
+	var alertLabel = document.createElement('label');
+	alertLabel.setAttribute('id', "alertLabel");
+	alertLabel.innerHTML = msg;
+	alertDiv.appendChild(alertLabel);
+
+	var alertBut = document.createElement('button');
+	alertBut.setAttribute('id', 'noteBut');
+	alertBut.setAttribute('class', 'btn btn-info pull-right');
+	alertBut.setAttribute('type', 'button');
+	alertBut.innerHTML = "Ok";
+	alertBut.onclick = function(){
+		$('#alerterDiv').remove();
+	}
+	alertDiv.appendChild(alertBut);
+
 }
